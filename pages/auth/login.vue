a7d0250eaab67c9024b173bd0cbda8cf62c06dea
rahhi555 <whowhose0904@gmail.com>
2021-08-19 02:19:54 +0900

    Add: login

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -15,38 +15,61 @@
               <div class="text-center pa-5">
                 <div class="text-h4 font-weight-bold white--text">Login</div>
                 <div class="mt-4">
-                  <v-btn class="ma-2" color="blue"> 
+                  <v-btn class="ma-2" color="blue">
                     <v-icon dark left> mdi-google </v-icon>Google
                   </v-btn>
                 </div>
               </div>
             </template>
+            <ValidationObserver v-slot="{ invalid }">
+              <v-card-text class="text-center">
+                <div class="text-center font-weight-light">Or Be Classical</div>
 
-            <v-card-text class="text-center">
-              <div class="text-center font-weight-light">Or Be Classical</div>
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  rules="email|required"
+                  name="メールアドレス"
+                >
+                  <v-text-field
+                    v-model="authValues.email"
+                    color="secondary"
+                    placeholder="Email..."
+                    :error-messages="errors[0]"
+                    :prepend-icon="
+                      errors.length == 0 && authValues.email.length >= 1
+                        ? 'mdi-email-outline'
+                        : 'mdi-email-open'
+                    "
+                  />
+                </ValidationProvider>
 
-              <v-text-field
-                class="mt-10"
-                color="secondary"
-                placeholder="Full Name..."
-                prepend-icon="mdi-face"
-              />
+                <ValidationProvider
+                  v-slot="{ errors }"
+                  rules="min:8|required"
+                  name="パスワード"
+                >
+                  <v-text-field
+                    v-model="authValues.password"
+                    class="mb-8"
+                    color="secondary"
+                    placeholder="Password..."
+                    :error-messages="errors[0]"
+                    :prepend-icon="
+                      errors.length == 0 && authValues.password.length >= 1
+                        ? 'mdi-lock-outline'
+                        : 'mdi-lock-off'
+                    "
+                    :append-icon="isShowPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="isShowPassword ? 'text' : 'password'"
+                    @click:append="isShowPassword = !isShowPassword"
+                  />
+                </ValidationProvider>
 
-              <v-text-field
-                color="secondary"
-                placeholder="Email..."
-                prepend-icon="mdi-email"
-              />
-
-              <v-text-field
-                class="mb-8"
-                color="secondary"
-                placeholder="Password..."
-                prepend-icon="mdi-lock-outline"
-              />
-
-              <v-btn color="accent" rounded text large> Let's Go </v-btn>
-            </v-card-text>
+                <v-btn :disabled="invalid" color="accent" rounded text large @click="login">
+                  Let's Go
+                </v-btn>
+              </v-card-text>
+            </ValidationObserver>
           </MaterialCard>
         </v-slide-y-transition>
       </v-col>
@@ -54,8 +77,40 @@
   </v-container>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, reactive, useRouter } from '@nuxtjs/composition-api'
+import firebase from '~/plugins/firebase'
+
+export default defineComponent({
   layout: 'unProtected',
-}
+  setup(){
+    const router = useRouter()
+
+    const authValues = reactive({
+      email: '',
+      password: '',
+    })
+
+    const isShowPassword = false
+
+    const login = (): void => {
+      window.$nuxt.$loading.start()
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(authValues.email, authValues.password)
+        .then(() => {
+          router.push('/')
+        })
+        .finally(() => {
+          window.$nuxt.$loading.finish()
+        })
+    }
+
+    return {
+      authValues,
+      isShowPassword,
+      login
+    }
+  }
+})
 </script>
