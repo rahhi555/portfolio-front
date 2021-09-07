d84978288983b2a73e013e7acd7ca201038be971
rahhi555 <whowhose0904@gmail.com>
2021-09-07 21:18:22 +0900

    Fix: node

--- components/protected/plans/PlanTableButton.vue
+++ components/protected/plans/PlanTableButton.vue
@@ -12,75 +12,110 @@
     >
       <v-tooltip bottom>
         <template #activator="{ on, attrs }">
-          <v-icon v-if="isMyPlan(item) && item.published" v-bind="attrs" v-on="on" v-text="'mdi-eye'" />
-          <v-icon v-else-if="isMyPlan(item) && !item.published" v-bind="attrs" v-on="on" v-text="'mdi-eye-remove'" />
+          <v-icon
+            v-if="isMyPlan(item) && item.published"
+            v-bind="attrs"
+            v-on="on"
+            v-text="'mdi-eye'"
+          />
+          <v-icon
+            v-else-if="isMyPlan(item) && !item.published"
+            v-bind="attrs"
+            v-on="on"
+            v-text="'mdi-eye-remove'"
+          />
+          <v-icon
+            v-else-if="!isMyPlan(item) && myMember(item) && myMember(item).accept"
+            v-bind="attrs"
+            v-on="on"
+            v-text="'mdi-eye-check-outline'"
+          />
           <v-icon v-else v-bind="attrs" v-on="on" v-text="'mdi-eye-outline'" />
         </template>
         <span v-if="isMyPlan(item) && item.published">マイ計画：公開</span>
-        <span v-else-if="isMyPlan(item) && !item.published">マイ計画：非公開</span>
-        <span v-else>公開</span>
-      </v-tooltip>
-    </app-btn>
-    <app-btn
-      v-else
-      color="error"
-      class="px-2 ml-1"
-      elevation="0"
-      min-width="0"
-      small
-      text
-    >
-      <v-tooltip bottom>
-        <template #activator="{ on, attrs }">
-          <v-icon v-bind="attrs" v-on="on" v-text="'mdi-eye-off-outline'" />
-        </template>
-        <span>非公開</span>
+        <span v-else-if="isMyPlan(item) && !item.published"
+          >マイ計画：非公開</span
+        >
+        <span v-else-if="!isMyPlan(item) && myMember(item) && myMember(item).accept">公開：承認済み</span>
+        <span v-else>公開：未承認</span>
       </v-tooltip>
     </app-btn>
 
-    <app-btn
-      v-if="!isMyPlan(item)"
-      color="success"
-      class="px-2 ml-1"
-      elevation="0"
-      min-width="0"
-      small
-      text
-      @click="joinRequest(item)"
-    >
+    <app-btn v-else class="px-2 ml-1" elevation="0" min-width="0" small text>
       <v-tooltip bottom>
         <template #activator="{ on, attrs }">
-          <v-icon v-bind="attrs" v-on="on" v-text="'mdi-import'" />
+          <v-icon
+            v-if="myMember(item) && myMember(item).accept"
+            color="success"
+            v-bind="attrs"
+            v-on="on"
+            @click="editPlan(item)"
+            v-text="'mdi-eye-remove-outline'"
+          />
+          <v-icon
+            v-else
+            color="error"
+            v-bind="attrs"
+            v-on="on"
+            v-text="'mdi-eye-remove-outline'"
+          />
         </template>
-        <span>参加リクエスト</span>
-      </v-tooltip>
-    </app-btn>    
-
-    <app-btn
-      v-if="isMyPlan(item)"
-      color="error"
-      class="px-2 ml-1"
-      elevation="0"
-      min-width="0"
-      small
-      text
-      @click="$emit('delete-handle')"
-    >
-      <v-tooltip bottom>
-        <template #activator="{ on, attrs }">
-          <v-icon v-bind="attrs" v-on="on" v-text="'mdi-delete-outline'" />
-        </template>
-        <span>削除</span>
+        <span v-if="myMember(item) && myMember(item).accept">非公開：承認済み</span>
+        <span v-else>非公開：未承認</span>
       </v-tooltip>
     </app-btn>
-    {{isMyMember(item)}}
+
+    <app-btn class="px-2 ml-1" elevation="0" min-width="0" small text>
+      <v-tooltip bottom>
+        <template #activator="{ on, attrs }">
+          <v-icon
+            v-if="!isMyPlan(item) && !myMember(item)"
+            color="grey"
+            v-bind="attrs"
+            v-on="on"
+            @click="joinRequest(item)"
+            v-text="'mdi-import'"
+          />
+          <v-icon
+            v-if="!isMyPlan(item) && myMember(item) && !myMember(item).accept"
+            color="warning"
+            v-bind="attrs"
+            v-on="on"
+            v-text="'mdi-import'"
+          />
+          <v-icon
+            v-if="!isMyPlan(item) && myMember(item) && myMember(item).accept"
+            color="success"
+            v-bind="attrs"
+            v-on="on"
+            v-text="'mdi-import'"
+          />
+          <v-icon
+            v-if="isMyPlan(item)"
+            color="error"
+            v-bind="attrs"
+            v-on="on"
+            @click="$emit('delete-handle')"
+            v-text="'mdi-delete-outline'"
+          />
+        </template>
+        <span v-if="!isMyPlan(item) && !myMember(item)">承認リクエスト</span>
+        <span v-if="!isMyPlan(item) && myMember(item) && !myMember(item).accept"
+          >承認待ち</span
+        >
+        <span v-if="!isMyPlan(item) && myMember(item) && myMember(item).accept"
+          >承認済み</span
+        >
+        <span v-if="isMyPlan(item)">削除</span>
+      </v-tooltip>
+    </app-btn>
   </span>
 </template>
 
 <script lang="ts">
 import { defineComponent, useRouter, useContext } from '@nuxtjs/composition-api'
-import { Plan } from 'interface'
-import { UserStore, SnackbarStore } from '~/store'
+import { Plan, Member } from 'interface'
+import { UserStore, SnackbarStore, PlansStore } from '~/store'
 
 export default defineComponent({
   props: {
@@ -102,8 +137,8 @@ export default defineComponent({
     }
 
     const joinRequest = (item: Plan) => {
-      if(!confirm('参加リクエストを送信してよろしいですか？')) {
-        alert('参加リクエストをキャンセルしました')
+      if (!confirm('承認リクエストを送信してよろしいですか？')) {
+        alert('承認リクエストをキャンセルしました')
         return
       }
 
@@ -111,20 +146,25 @@ export default defineComponent({
 
       $axios
         .$post(`/api/v1/plans/${item.id}/members`, { member })
+        .then(member => PlansStore.addMember({id: item.id, member}))
         .catch(() => SnackbarStore.catchError())
-        .finally(() => SnackbarStore.CRUDvisible({ model: '参加リクエスト', crud: 'create' }))
+        .finally(() =>
+          SnackbarStore.CRUDvisible({ model: '承認リクエスト', crud: 'create' })
+        )
     }
 
-    const isMyMember = (item: Plan) => {
+    const myMember = (item: Plan): Member | null => {
       const { members } = item
-      return members.filter(member => member.userId === UserStore.currentUser.id)
+      return members.filter(
+        (member) => member.userId === UserStore.currentUser.id
+      )[0]
     }
 
     return {
       isMyPlan,
       editPlan,
       joinRequest,
-      isMyMember
+      myMember,
     }
   },
 })
--- store/plans.ts
+++ store/plans.ts
@@ -1,6 +1,9 @@
 import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
-import { Plan } from 'interface'
+import { Plan, Member } from 'interface'
 import { $axios } from '~/utils/axios-accessor'
+import { SnackbarStore, UserStore } from '~/utils/store-accessor'
+
+const MODEL = '計画'
 
 @Module({
   name: 'plans',
@@ -20,6 +23,11 @@ export default class Plans extends VuexModule {
     return this.plansState
   }  
 
+  public get currentUserMember() {
+    const members = this.plansState?.flatMap(plan => plan.members)
+    return members?.filter(member => member.userId === UserStore.currentUser.id)
+  }
+
   @Mutation
   private setCurrentPlanMutation(plan: Plan) {
     this.currentPlanState = plan
@@ -30,6 +38,26 @@ export default class Plans extends VuexModule {
     this.plansState = plans
   }
 
+  @Mutation
+  private addPlansMutation(plan: Plan) {
+    this.plansState?.push(plan)
+  }
+
+  @Mutation
+  public addMember({id, member}: {id:number, member:Member}) {
+    const index = this.plansState!.findIndex(plan => plan.id === id)
+    console.log('index', index)
+    console.log('target', this.plansState[index])
+    this.plansState[index].members.push(member)
+    console.log('target', this.plansState[index])
+  }
+
+  @Mutation
+  private deletePlansMutation(id: number) {
+    const index = this.plansState!.findIndex(plan => plan.id === id)
+    this.plansState?.splice(index, 1)
+  }
+
   @Action({ rawError: true })
   public async setCurrentPlan(planId: string) {
     if(this.currentPlanState?.id === Number.parseInt(planId)) return
@@ -38,8 +66,31 @@ export default class Plans extends VuexModule {
   }
 
   @Action({ rawError: true })
-  public async setPlans() {
+  public async indexPlans() {
     const plans = await $axios.$get(`/api/v1/plans`)
     this.setPlansMutation(plans)
+  }
+
+  @Action({ rawError: true })
+  public async createPlan({ name, published }: { name: string, published: boolean }) {
+    const plan = { name, published }
+    await $axios
+      .$post('/api/v1/plans', { plan })
+      .then((post) => this.addPlansMutation(post))
+      .catch(() => SnackbarStore.catchError())
+      .finally(() => SnackbarStore.CRUDvisible({model: MODEL, crud: 'create'}))
+  }
+
+  @Action({ rawError: true })
+  public async deletePlan(plan: Plan) {
+    if(!confirm(`計画 ${plan.name} を削除してもよろしいですか？`)) {
+      alert('キャンセルしました')
+      return
+    }
+    await $axios
+      .$delete(`/api/v1/plans/${plan.id}`)
+      .then(() => this.deletePlansMutation(plan.id))
+      .catch(() => SnackbarStore.catchError())
+      .finally(() => SnackbarStore.CRUDvisible({model: MODEL, crud: 'delete'})) 
   }
 }
