7a565ff5a306525443c2252a2da68b67d6690229
rahhi555 <whowhose0904@gmail.com>
2021-12-26 00:48:36 +0900

    Fix: firebase.analyticsのバグ修正

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -4,34 +4,31 @@ import 'firebase/analytics'
 import { UserStore } from '~/store'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP38Ubcnjfzfg2IN4PTexqi21NJ8Kdwbo",
-  authDomain: "minimap-dadd2.firebaseapp.com",
-  projectId: "minimap-dadd2",
-  storageBucket: "minimap-dadd2.appspot.com",
-  messagingSenderId: "251879137678",
-  appId: "1:251879137678:web:1bb63f681e18329d8e04df",
-  measurementId: "G-YC8GZGYH1W"
-};
+  apiKey: 'AIzaSyDP38Ubcnjfzfg2IN4PTexqi21NJ8Kdwbo',
+  authDomain: 'minimap-dadd2.firebaseapp.com',
+  projectId: 'minimap-dadd2',
+  storageBucket: 'minimap-dadd2.appspot.com',
+  messagingSenderId: '251879137678',
+  appId: '1:251879137678:web:1bb63f681e18329d8e04df',
+  measurementId: 'G-YC8GZGYH1W',
+}
 
-if(!firebase.apps.length){
+if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
-}
 
-if (process.env.NODE_ENV !== "production") {
-  const auth = firebase.auth()
-  // @ts-ignore
-  auth.useEmulator('http://localhost:9099', { disableWarnings: true })
-}
-
-if (process.env.NODE_ENV === "production") {
-  firebase.analytics()
+  console.log(process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'production') {
+    firebase.analytics()
+  } else {
+    const auth = firebase.auth()
+    // @ts-ignore
+    auth.useEmulator('http://localhost:9099', { disableWarnings: true })
+  }
 }
 
 firebase.auth().onAuthStateChanged((user) => {
   if (!user || !user.email) return
   UserStore.setEmail(user.email)
 })
-
-
 
 export default firebase
